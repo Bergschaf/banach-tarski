3cc38282df3d2691edeb6c6c491078cb4dd1ad86
bergschaf <christian.k@netcom-mail.de>
2023-12-24 18:01:11 +0100

    commit

--- Pythagoras.lean
+++ Pythagoras.lean
@@ -1,6 +1,5 @@
 import Mathlib.Data.Real.Basic
 
-
 structure complex where
   re : Real
   im : Real
@@ -21,5 +20,49 @@ instance : Neg complex :=
 instance : Mul complex :=
   ⟨fun x y  ↦   ⟨ x.re * y.re - x.im * y.im, x.re * y.im + x.im * y.re⟩⟩
 
-#check ∑ i in range 1, n
-theorem kleiner_gauss (n i: Nat) : (∑ n in range 1, n) = n * (n + 1) / 2:=
+
+
+def sum_up_to (n : Nat): ℕ :=
+  match n with
+    | 0 => 0
+    | (Nat.succ n) => sum_up_to n + Nat.succ n
+
+#eval sum_up_to 3
+
+#eval 5 / 2
+
+theorem kleiner_gauss (n: Nat) : sum_up_to n * 2 = n * (n + 1) := by
+  induction n with
+    | zero => simp
+    | succ d hd => 
+    rw [sum_up_to]
+    rw [Nat.succ_eq_add_one]
+    rw [Nat.add_mul]
+    rw [Nat.mul_add]
+    rw [Nat.mul_one]
+   
+    rw [Nat.add_mul d 1 (d + 1)]
+    simp
+    rw [add_assoc]
+    rw [← Nat.mul_two (d + 1)]
+    simp
+    use hd
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+  
+
+
+
+
