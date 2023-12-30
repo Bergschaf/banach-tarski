2771b81cba9d6590df356279b3dfe95c51a5ba5d
Chma113 <154738891+Chma113@users.noreply.github.com>
2023-12-30 16:48:39 +0100

    Update Pythagoras.lean

--- Pythagoras.lean
+++ Pythagoras.lean
@@ -38,20 +38,21 @@ theorem kleiner_gauss (n: Nat) :
   sum_up_to n * 2 = n * (n + 1) := by
 
   induction n with
-    | zero => simp
+    | zero => 
+    simp
+    rw [sum_up_to]
     | succ d hd => 
     rw [sum_up_to]
     rw [Nat.succ_eq_add_one]
     rw [Nat.add_mul]
     rw [Nat.mul_add]
     rw [Nat.mul_one]
-    
     rw [Nat.add_mul d 1 (d + 1)]
     simp
     rw [add_assoc]
     rw [← Nat.mul_two (d + 1)]
     simp
-    use hd
+    apply hd
 
 
 
