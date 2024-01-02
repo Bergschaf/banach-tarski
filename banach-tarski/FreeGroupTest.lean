ec5fc4536c4b65e621d2a005cfbff749cf132689
Bergschaf <christian.k@netcom-mail.de>
2024-01-02 18:07:08 +0100

    List.prod

--- banach-tarski/FreeGroupTest.lean
+++ banach-tarski/FreeGroupTest.lean
@@ -70,89 +70,94 @@ theorem sq_2_mul_sq_2_eq_2 : Real.sqrt 2 * Real.sqrt 2 = 2 := by
   norm_num
 
 set_option maxHeartbeats 200000
-def x : ℚ := 5/3
-def y : ℚ := 3
-#eval x
 
-abbrev matrix_3x3_q := Matrix (Fin 3) (Fin 3) ℚ
-
-def matrix_a : matrix_3x3_q := !![3/5, -4/5, 0; 4/5, 3/5,0; 0, 0, 1]
-def matrix_a' : matrix_3x3_q := !![3/5, 4/5, 0; -4/5, 3/5, 0; 0, 0, 1]
-def matrix_b : matrix_3x3_q:= !![1,0,0;0,3/5,-4/5;0, 4/5, 3/5]
-def matrix_b' :matrix_3x3_q := !![1,0, 0;0, 3/5, 4/5; 0, -4/5, 3/5 ]
-def matrix_one : matrix_3x3_q := !![1, 0, 0; 0, 1, 0; 0, 0, 1]
+noncomputable section
+def matrix_a : Matrix (Fin 3) (Fin 3) Real := !![1, 0, 0; 0, 1/3,-2/3*Real.sqrt 2; 0, 2/3*Real.sqrt 2, 1/3]
+def matrix_a' : Matrix (Fin 3) (Fin 3) Real := !![1, 0, 0; 0, 1/3,2/3*Real.sqrt 2; 0, -2/3*Real.sqrt 2, 1/3]
+def matrix_b : Matrix (Fin 3) (Fin 3) Real := !![1/3, -2/3*Real.sqrt 2, 0;( 2/3*Real.sqrt 2),1/3, 0; 0, 0, 1]
+def matrix_b' :Matrix (Fin 3) (Fin 3) Real := !![1/3, 2/3*Real.sqrt 2, 0;( -2/3*Real.sqrt 2),1/3, 0; 0, 0, 1]
+def matrix_one : Matrix (Fin 3) (Fin 3) Real := !![1, 0, 0; 0, 1, 0; 0, 0, 1]
 ---def matritzen : Set (Matrix (Fin 3) (Fin 3) Real) := {matrix_a, matrix_b, matrix_a', matrix_b', matrix_one}
 
 
-theorem matrix_inverse_a :  matrix_a * matrix_a' = matrix_one := by
-    rw [matrix_a]
-    rw [matrix_a']
-    rw [matrix_one]
-    norm_num
-
-theorem matrix_inverse_b :  matrix_b * matrix_b' = matrix_one := by
-    rw [matrix_b]
-    rw [matrix_b']
-    rw [matrix_one]
-    norm_num
-
-
-#eval matrix_a * matrix_a'
-#eval matrix_b * matrix_b'
-
-
-def matrix_ab : Set matrix_3x3_q := {matrix_a, matrix_b, matrix_a', matrix_b'}
-abbrev ab_subtype :=  {w : matrix_3x3_q | w ∈ matrix_ab}
-
-
-def G := Group matrix_ab
-
-abbrev r_3 := Fin 3 -> ℚ
-
-def one_zero_zero : r_3 := ![4,0,0]
-
-
-def fold (word : List ab_subtype) : matrix_3x3_q :=
-  List.foldl (fun matrix1 matrix2=> matrix1 * matrix2) 1 word
-
-
-theorem fold_zero_eq_one (word : List ab_subtype) (h : word.length = 0) : fold word = 1 := by
-  rw [fold]
-  sorry
-
-theorem fold_p_n (word: List ab_subtype) (n : Nat) (h: word.length = n) :
-   ∃ w : ab_subtype, fold word = (fold (word.take (n - 1))) *  w := by
-  rw [fold]
-  rw [fold]
-  simp
-  rw [← h]
+theorem matrix_inverse :  matrix_a * matrix_a' = matrix_one := by
   sorry
 
 
-def rotate (word : List ab_subtype) (vec : r_3) : r_3 :=
-  (fold word).vecMul vec
+
+theorem matrix_a_det_neq_zero : Matrix.det matrix_a ≠ 0 := by
+  norm_num
+  refine Matrix.nondegenerate_iff_det_ne_zero.mp ?_
+  sorry
+
+theorem matrix_a'_det_neq_zero : Matrix.det matrix_a' ≠ 0 := by
+  norm_num
+  refine Matrix.nondegenerate_iff_det_ne_zero.mp ?_
+  sorry
 
 
-def a_b_c_vec (a b c : ℚ) (n : Nat) : r_3 :=
-  (1/5)^n * ![4 * a, 3 * b, -4 * c]
+theorem matrix_b_det_neq_zero : Matrix.det matrix_b ≠ 0 := by
+  norm_num
+  refine Matrix.nondegenerate_iff_det_ne_zero.mp ?_
+  sorry
 
-theorem lemma_3_1 (p: List ab_subtype) (a b c: ℤ) (n : Nat) (h: n = p.length):
-       ∃ a b c, rotate p one_zero_zero = a_b_c_vec a b c n:= by
+theorem matrix_b'_det_neq_zero : Matrix.det matrix_b' ≠ 0 := by
+  norm_num
+  refine Matrix.nondegenerate_iff_det_ne_zero.mp ?_
+  sorry
+
+theorem matrix_one_det_neq_zero : Matrix.det matrix_one ≠ 0 := by
+  norm_num
+  refine Matrix.nondegenerate_iff_det_ne_zero.mp ?_
+  sorry
+
+def gl_a  : GL (Fin 3) Real := Matrix.GeneralLinearGroup.mkOfDetNeZero matrix_a matrix_a_det_neq_zero
+def gl_b  : GL (Fin 3) Real := Matrix.GeneralLinearGroup.mkOfDetNeZero matrix_b matrix_b_det_neq_zero
+def gl_a'  : GL (Fin 3) Real := Matrix.GeneralLinearGroup.mkOfDetNeZero matrix_a' matrix_a'_det_neq_zero
+def gl_b'  : GL (Fin 3) Real := Matrix.GeneralLinearGroup.mkOfDetNeZero matrix_b' matrix_b'_det_neq_zero
+def gl_one  : GL (Fin 3) Real := Matrix.GeneralLinearGroup.mkOfDetNeZero matrix_one matrix_one_det_neq_zero
+
+
+def a_inv_a' :=
+  gl_a * gl_a' = matrix_one
+
+def b_inv_b' :=
+  gl_b * gl_b' = matrix_one
+
+def gl_ab : Set (GL (Fin 3) Real) := {gl_a, gl_a', gl_b, gl_b'}
+abbrev gl_subtype :=  {w : GL (Fin 3) Real | w ∈ gl_ab}
+
+
+def G := Subgroup.closure {gl_a, gl_b}
+
+abbrev r_3 := Fin 3 -> ℝ
+
+def zero_one_zero : r_3 := ![0,1,0]
+def gl_to_m (matrix: GL (Fin 3) Real) : Matrix (Fin 3) (Fin 3) Real := matrix
+
+
+
+def rotate (word : List gl_subtype) (vec : r_3) : r_3 :=
+  (gl_to_m (List.prod word)).vecMul vec
+
+
+def a_b_c_vec (a b c : Real) (n : Nat) : r_3 :=
+  1/3^n * ![a * Real.sqrt 2, b, c * Real.sqrt 2]
+
+theorem lemma_3_1 (p: List gl_subtype) (a b c: Real) (n : Nat) (h: n = p.length):
+       ∃ a b c, rotate p zero_one_zero = a_b_c_vec a b c n:= by
 
   induction n with
     | zero =>
     rw [rotate]
     simp
-    rw [one_zero_zero]
-    use 1
     use 0
+    use 1
     use 0
     rw [a_b_c_vec]
     simp
-    rw [fold_zero_eq_one]
-    norm_num
-    apply symm
-    exact h
+    rw [gl_to_m]
+    rw?
 
     | succ d hd =>
     rw [rotate]
@@ -160,11 +165,7 @@ theorem lemma_3_1 (p: List ab_subtype) (a b c: ℤ) (n : Nat) (h: n = p.length):
       apply symm
       rw [← Nat.succ_eq_add_one]
       exact h
-    rw [one_zero_zero]
-    rw [fold]
-
-
-
+    apply?
 
 
 
