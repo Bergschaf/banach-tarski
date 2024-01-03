49bd166934e3e0d3008dcdb36eb9d02359e3a531
Bergschaf <christian.k@netcom-mail.de>
2024-01-03 13:55:16 +0100

    cleanup

--- banach-tarski/FreeGroupTest.lean
+++ banach-tarski/FreeGroupTest.lean
@@ -1,76 +1,17 @@
 import Mathlib.Tactic
 import Mathlib.Algebra.Group.Basic
 import Mathlib.GroupTheory.Subgroup.Basic
-import Mathlib.GroupTheory.FreeGroup.Basic
-import Mathlib.GroupTheory.FreeGroup.IsFreeGroup
 import Mathlib.Data.Matrix.Notation
 import Mathlib.Data.Matrix.Basic
 import Mathlib.Data.Nat.Basic
 import Mathlib.Data.Real.Basic
 import Mathlib.Data.Real.Sqrt
-import Mathlib.Data.Set.Card
 
 import Mathlib.LinearAlgebra.Matrix.NonsingularInverse
 import Mathlib.LinearAlgebra.Matrix.GeneralLinearGroup
 
 set_option maxHeartbeats 0
-inductive Generators where
-  | a : Generators
-  | b : Generators
-  deriving DecidableEq
-open Generators
 
---noncomputable def matrix_a := Matrix.smul_of (1/3) !![3, 0, 0; 0, 1,-2*Real.sqrt 2; 0, 2*Real.sqrt 2, 1]
---noncomputable def matrix_a' := Matrix.smul_of (1/3) !![1, -2*Real.sqrt 2, 0;( 2*Real.sqrt 2),1, 0; 0, 0, 3]
---noncomputable def matrix_b := Matrix.smul_of (1/3) !![3, 0, 0; 0, 1,-2*Real.sqrt 2; 0, 2*Real.sqrt 2, 1]
---noncomputable def matrix_b' := Matrix.smul_of (1/3) !![1, -2*Real.sqrt 2, 0;( -2*Real.sqrt 2),1, 0; 0, 0, 3]
---noncomputable def generator_value (g : (Generators ×  Bool)) :=
---  match g with
---   | (Generators.a, true) => matrix_a
---   | (Generators.b, true) => matrix_a'
---   | (Generators.a, false) => matrix_b
---   | (Generators.b, false) => matrix_b'
-
-
---theorem: FreeGroup.toWord_one{α : Type u} [DecidableEq α] :
---  FreeGroup.toWord 1 = []
-def f2 := {w: FreeGroup Generators | true}
-def e := {w: FreeGroup Generators | (FreeGroup.toWord w) = []}
-def s_a := {w : FreeGroup Generators  | (FreeGroup.toWord w).head? = .some (Generators.a, true)}
-def s_b := {w : FreeGroup Generators  | (FreeGroup.toWord w).head? = .some (Generators.b, true)}
-def s_a' := {w : FreeGroup Generators  | (FreeGroup.toWord w).head? = .some (Generators.a, false)}
-def s_b' := {w : FreeGroup Generators  | (FreeGroup.toWord w).head? = .some (Generators.b, false)}
-
-
-theorem about_s_a (z : FreeGroup Generators) : z ∈ s_a -> ((FreeGroup.toWord z).head? = .some (Generators.a, true)) := by
-  intro x
-  use x
-#check f2
-
-theorem s_a_in_f2 : x ∈ s_a -> x ∈ f2 := by
-  intro h
-  sorry
-
-
-theorem union_f2 : s_a ∪ s_b ∪ s_a' ∪ s_b' ∪ e = f2 := by
-  apply subset_antisymm
-  intro x h
-  sorry
-  sorry
-
-
-
----theorem s_a_in_FreeGroup (s := s_a) (w : FreeGroup Generators) : (FreeGroup.toWord w).head? = (Generators.a, true) -> w ∈ s_a :=
-
----  sorry
-
---------------------
-
---def r_3_set := Set r_3
-theorem sq_2_mul_sq_2_eq_2 : Real.sqrt 2 * Real.sqrt 2 = 2 := by
-  norm_num
-
-set_option maxHeartbeats 0
 
 noncomputable section
 def matrix_a : Matrix (Fin 3) (Fin 3) Real := !![1, 0, 0; 0, 1/3,-2/3*Real.sqrt 2; 0, 2/3*Real.sqrt 2, 1/3]
@@ -80,11 +21,17 @@ def matrix_b' :Matrix (Fin 3) (Fin 3) Real := !![1/3, 2/3*Real.sqrt 2, 0;( -2/3*
 def matrix_one : Matrix (Fin 3) (Fin 3) Real := 1
 ---def matritzen : Set (Matrix (Fin 3) (Fin 3) Real) := {matrix_a, matrix_b, matrix_a', matrix_b', matrix_one}
 
-
 theorem matrix_inverse :  matrix_a * matrix_a' = matrix_one := by
+  rw [matrix_a]
+  rw [matrix_a']
+  rw [matrix_one]
+  norm_num
+  ext h1 h2
+  fin_cases h1
+  simp
   sorry
-
-
+  sorry
+  sorry
 
 theorem matrix_a_det_neq_zero : Matrix.det matrix_a ≠ 0 := by
   norm_num
@@ -117,8 +64,6 @@ def gl_b  : GL (Fin 3) Real := Matrix.GeneralLinearGroup.mkOfDetNeZero matrix_b 
 def gl_a'  : GL (Fin 3) Real := Matrix.GeneralLinearGroup.mkOfDetNeZero matrix_a' matrix_a'_det_neq_zero
 def gl_b'  : GL (Fin 3) Real := Matrix.GeneralLinearGroup.mkOfDetNeZero matrix_b' matrix_b'_det_neq_zero
 def gl_one  : GL (Fin 3) Real := Matrix.GeneralLinearGroup.mkOfDetNeZero matrix_one matrix_one_det_neq_zero
-
-
 
 
 def a_inv_a' :=
@@ -218,7 +163,6 @@ theorem case_b (x) (h: x = gl_b): ∃ a b c n, rotate_g x zero_one_zero = a_b_c_
     norm_num
 
 
-
 def hk (x : GL (Fin 3) Real) (h: x ∈ erzeuger): ∃ a b c n, rotate_g x zero_one_zero = a_b_c_vec a b c n := by
  cases h with
  | inl ha =>
@@ -256,8 +200,6 @@ theorem h_mul (x y) (h1: ∃ a b c n, rotate_g x zero_one_zero = a_b_c_vec a b c
 
   rcases h1 with ⟨a, b, c, n, h1'⟩
   rcases h2 with ⟨e, f, g, m, h2'⟩
-
-
   sorry
 
 
@@ -274,26 +216,10 @@ def h_inv (x) (h1:∃ a b c n, rotate_g x zero_one_zero = a_b_c_vec a b c n) :
 
   rcases h1 with ⟨a, b, c, n, h1'⟩
   simp
-
-
-
-
-
+  sorry
 
 theorem lemma_3_1 (p: GL (Fin 3) Real) (h: p ∈ G):
        ∃ a b c n, rotate_g p zero_one_zero = a_b_c_vec a b c n:=
   Subgroup.closure_induction h hk h_one h_mul h_inv
 
 #check lemma_3_1
-
-
-
-
-
-
-
-
----def matrix_group := {w: GL (Fin 3) Real | true}
-
-theorem matritzen_in_GL : ∀ x ∈ matritzen, x ∈ matrix_group :=
-  sorry
