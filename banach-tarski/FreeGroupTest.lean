e7757968041520f809acf5940195b3c4b7b0dd68
bergschaf <christian.k@netcom-mail.de>
2024-01-04 10:29:36 +0100

    Matrix.det_ne_zero

--- banach-tarski/FreeGroupTest.lean
+++ banach-tarski/FreeGroupTest.lean
@@ -18,8 +18,9 @@ def matrix_a'  : Matrix (Fin 3) (Fin 3) Real := !![1, 0, 0; 0, 1/3, 2/3*Real.sqr
 def matrix_b   : Matrix (Fin 3) (Fin 3) Real := !![1/3, -2/3*Real.sqrt 2, 0; (2/3*Real.sqrt 2), 1/3, 0; 0, 0, 1]
 def matrix_b'  : Matrix (Fin 3) (Fin 3) Real := !![1/3, 2/3*Real.sqrt 2, 0; (-2/3*Real.sqrt 2), 1/3, 0; 0, 0, 1]
 def matrix_one : Matrix (Fin 3) (Fin 3) Real := 1
+end noncomputable section
 
-theorem matrix_inverse :  matrix_a * matrix_a' = matrix_one := by
+theorem matrix_a_inverse :  matrix_a * matrix_a' = matrix_one := by
   rw [matrix_a]
   rw [matrix_a']
   rw [matrix_one]
@@ -27,16 +28,45 @@ theorem matrix_inverse :  matrix_a * matrix_a' = matrix_one := by
   ext h1 h2
   fin_cases h1
   simp
-  sorry
-  sorry
-  sorry
-end section
-theorem matrix_a_det_neq_zero : Matrix.det matrix_a ≠ 0 := by
+  rw [@Matrix.one_fin_three]
+  simp
+
+  simp
   norm_num
-  rw [Matrix.det_apply]
-  sorry
+  rw [@mul_mul_mul_comm]
+  norm_num
+  simp
+  rw [div_eq_mul_inv]
+  rw [← mul_assoc]
+  rw [← mul_assoc]
+  rw [@Mathlib.Tactic.RingNF.add_neg]
+  rw [@mul_assoc]
+  rw [mul_comm 3⁻¹]
+  rw [mul_comm 3⁻¹]
+  rw [← mul_assoc]
+  rw [sub_self]
+  rw [@Matrix.one_fin_three]
+  simp
 
+  simp
+  ring
+  simp
+  rw [Real.sq_sqrt]
+  ring
+  rw [@Matrix.one_fin_three]
+  exact rfl
+  norm_num
 
+theorem matrix_a_det_neq_zero : Matrix.det matrix_a ≠ 0 := by
+  rw [matrix_a]
+  rw [Matrix.det_fin_three]
+  simp
+  norm_num
+  ring
+  simp
+  rw [Real.sq_sqrt]
+  norm_num
+  norm_num
 
 theorem matrix_a'_det_neq_zero : Matrix.det matrix_a' ≠ 0 := by
   norm_num
@@ -65,6 +95,10 @@ def gl_b   : GL (Fin 3) Real := Matrix.GeneralLinearGroup.mkOfDetNeZero matrix_b
 def gl_b'  : GL (Fin 3) Real := Matrix.GeneralLinearGroup.mkOfDetNeZero matrix_b' matrix_b'_det_neq_zero
 def gl_one : GL (Fin 3) Real := Matrix.GeneralLinearGroup.mkOfDetNeZero matrix_one matrix_one_det_neq_zero
 end noncomputable section
+
+theorem a_inv_a' : gl_a * gl_a' = 1 := by
+  sorry
+
 
 def a_inv_a' :=
   gl_a * gl_a' = matrix_one
@@ -210,4 +244,3 @@ def h_inv (x) (h1:∃ a b c : ℤ, ∃ n : ℕ, rotate x zero_one_zero = a_b_c_v
 theorem lemma_3_1 (p: GL (Fin 3) Real) (h: p ∈ G):
        ∃ a b c : ℤ, ∃ n : ℕ, rotate p zero_one_zero = a_b_c_vec a b c n:=
   Subgroup.closure_induction h hk h_one h_mul h_inv
-
