a422a620cd331e9e4c178e4a3622cf0a3acd4e0e
Bergschaf <christian.k@netcom-mail.de>
2024-01-21 03:16:46 +0100

    Update ./banach_tarski/Lemma_3_2.lean

--- banach_tarski/Lemma_3_2.lean
+++ banach_tarski/Lemma_3_2.lean
@@ -1,2 +1,14 @@
 import banach_tarski.Lemma_3_1
 #check rotate
+
+
+theorem freeGroup (x: GL (Fin 3) Real) (h: x ∈ G) (n: Nat):
+  rotate x zero_one_zero = a_b_c_vec 0 (3^n) 0 n -> n = 0 := by
+    rw [zero_one_zero]
+
+    intro h1
+
+    rw [a_b_c_vec] at h1
+    simp at h1
+    sorry
+  
