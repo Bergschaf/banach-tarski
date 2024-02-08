dd84a407f4f1849cf33babef569d56d7069e3188
bergschaf <christian.k@netcom-mail.de>
2024-02-08 16:37:46 +0100

    rm kugel.lean

--- banach_tarski/Kugel.lean
+++
@@ -1,7 +0,0 @@
-import Mathlib.Topology.MetricSpace.PseudoMetric
-import Mathlib.Data.Real.Basic
-import banach_tarski.Lemma_3_1
-
-def L := {x: r_3 | x ∈ Metric.ball 0 1}
-def origin : r_3 := ![0,0,0]
-def L' := L \ {origin}
--- banach_tarski/Orbit.lean
+++ banach_tarski/Orbit.lean
@@ -1,5 +1,8 @@
-import banach_tarski.Kugel
 import banach_tarski.Lemma_3_1
+
+def L := {x: r_3 | x ∈ Metric.ball 0 1}
+def origin : r_3 := ![0,0,0]
+def L' := L \ {origin}
 
 def same_orbit (a b: r_3) := ∃ p : G, rotate p a = b
 
