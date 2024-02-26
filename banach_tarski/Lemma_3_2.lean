b5d281a1daa0188b7ef4e61344c480f47eaa88fa
bergschaf <christian.k@netcom-mail.de>
2024-02-26 19:16:55 +0100

    commit

--- banach_tarski/Lemma_3_2.lean
+++ banach_tarski/Lemma_3_2.lean
@@ -1,8 +1,9 @@
 import banach_tarski.Lemma_3_1
 #check rotate
 
+variable (n1 : Nat)
 
-theorem freeGroup (x: GL (Fin 3) Real) (h: x ∈ G) (n: Nat):
+theorem freeGroup (x: GL (Fin 3) Real) (h: x ∈ G) (n: Nat) (h4 : n1 > 0):
   rotate x zero_one_zero = a_b_c_vec 0 (3^n) 0 n -> false := by
     rw [zero_one_zero]
 
@@ -10,6 +11,7 @@ theorem freeGroup (x: GL (Fin 3) Real) (h: x ∈ G) (n: Nat):
 
     rw [rotate]
 
+    --cases general_word_form_exists x h
 
     rcases general_word_form_exists x h with ⟨a1, b1, c1, d1, e1, f1, g1, h1, i1, n1, h2⟩
 
