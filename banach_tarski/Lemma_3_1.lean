45543668b44abd1f4eab19b7c01a49387a4a1801
bergschaf <christian.k@netcom-mail.de>
2024-02-29 21:36:35 +0100

    words as list

--- banach_tarski/Lemma_3_1.lean
+++ banach_tarski/Lemma_3_1.lean
@@ -14,7 +14,7 @@ import Mathlib.Data.Matrix.Reflection
 
 import banach_tarski.Definitions
 
-import banach_tarski.general_word_form
+--import banach_tarski.general_word_form
 
 def coe_gl_one_eq_one : ↑gl_one = 1 := by
   exact Units.val_eq_one.mp rfl
@@ -31,29 +31,6 @@ noncomputable def a_b_c_vec (a b c : ℤ) (n : Nat) : r_3 :=
 
 theorem x_inv_in_g (x: GL (Fin 3) Real) (h: x ∈ G): x⁻¹ ∈ G := by
   exact Subgroup.inv_mem G h
-
-theorem general_word_form_abc (a b c d e f g h i: ℤ) (n : Nat):
-  ∃ l m o p, Matrix.vecMul zero_one_zero (general_word_form a b c d e f g h i n) =
-    a_b_c_vec l m o p := by
-    rw [general_word_form]
-    use d
-    use e
-    use f
-    use n
-    rw [a_b_c_vec]
-    rw [zero_one_zero]
-
-    ext h1
-    fin_cases h1
-    simp
-    ring
-
-    simp
-    ring
-
-    simp
-    ring
-
 
 
 
@@ -112,96 +89,47 @@ def G_one : G := 1
 theorem h_one : ∃ a b c : ℤ, ∃ n : ℕ, rotate G_one zero_one_zero = a_b_c_vec a b c n := by
   apply case_one
 
-theorem vec_mul_abc_eq_abc (x : GL (Fin 3) Real) (h1: x ∈ G)
-  (h: ∃ j k l : ℤ, ∃ i : ℕ, rotate x zero_one_zero = a_b_c_vec j k l i) (a b c : ℤ) (n : Nat) :
-  ∃ e f g : ℤ , ∃ m : Nat, Matrix.vecMul (a_b_c_vec a b c n) x = a_b_c_vec e f g m := by
-  rw [a_b_c_vec]
-  rcases general_word_form_exists x h1 with ⟨a1, b1, c1, d1, e1, f1, g1, h1, i1,n1, h2⟩
-  rw [h2]
-  rw [general_word_form]
-  simp
-  use a * a1 + b * d1 + c * g1
-  use a * b1 * 2 + c * h1 * 2 + b * e1
-  use  a * c1 + b * f1 + c * i1
-  use n + n1
-  rw [a_b_c_vec]
-  ext hx
-  fin_cases hx
 
-  simp
-  ring
-
-  simp
-  ring!
-  simp
-  norm_num
-  ring
-  norm_num
-  ring
+variable (x : G_List)
 
 
-theorem h_mul (x : GL (Fin 3) Real) (hx: x ∈ G) (y: GL (Fin 3) Real) (hy: y ∈ G) (h1:∃ a b c : ℤ, ∃ n : ℕ, rotate x zero_one_zero = a_b_c_vec a b c n)
-(h2 :∃ d e f : ℤ, ∃ m : ℕ, rotate y zero_one_zero = a_b_c_vec d e f m):
-∃ g h i : ℤ, ∃ o : ℕ, rotate (x*y) zero_one_zero = a_b_c_vec g h i o := by
-  rw [rotate]
-  rw [@Matrix.GeneralLinearGroup.coe_mul]
-  rw [zero_one_zero]
+theorem lemma_3_1 {n : Nat} (p : List (erzeuger_t × Bool))  (h: List.length p = n):
+  ∃ a b c : ℤ, rotate (list_to_matrix p) zero_one_zero = a_b_c_vec a b c n := by
 
-  rw [rotate] at h1
-  rw [zero_one_zero] at h1
+    induction p with
+    | nil =>
+      use 0
+      use 1
+      use 0
+      rw [list_to_matrix]
+      rw [rotate]
+      rw [coe_gl_one_eq_one]
+      simp [zero_one_zero, a_b_c_vec]
+      rw [← h]
+      simp
 
-  rw [rotate] at h2
-  rw [zero_one_zero] at h2
-
-  rcases h1 with ⟨a, b, c, n, h1'⟩
-  rcases h2 with ⟨e, f, g, m, h2'⟩
-  rw [← @Matrix.vecMul_vecMul]
-  rw [h1']
-  apply vec_mul_abc_eq_abc
-
-  apply hy
-
-  rw [rotate]
-  rw [zero_one_zero]
-  rw [h2']
-  use e
-  use f
-  use g
-  use m
+    | cons head tail ih =>
+      cases head with
+      | _ => sorry
 
 
-def h_inv (x : GL (Fin 3) Real) (hx: x ∈ G)  (h1:∃ a b c : ℤ, ∃ n : ℕ, rotate x zero_one_zero = a_b_c_vec a b c n):
-  ∃ a b c n, rotate (x⁻¹) zero_one_zero = a_b_c_vec a b c n := by
-
-  rw [rotate]
-  rw [zero_one_zero]
-
-  rw [rotate] at h1
-  rw [zero_one_zero] at h1
-
-  rcases h1 with ⟨a, b, c, n, h1'⟩
-  simp
-
-  rcases general_word_form_exists (x)⁻¹ (x_inv_in_g x hx) with ⟨a1, b1, c1, d1, e1, f1, g1,hh1,  i1, n1, h2⟩
-
-  rw [← @Matrix.coe_units_inv]
-
-  rw [h2]
-
-  apply general_word_form_abc
 
 
-theorem h_s (x : GL (Fin 3) Real) (h : x ∈ erzeuger) :
-   ∃ a b c n, rotate x zero_one_zero = a_b_c_vec a b c n := by
-    cases h with
-    | inl ha =>
-    apply case_a
-    exact ha
-    | inr hb =>
-      apply case_b
-      exact hb
 
 
-theorem lemma_3_1 (p: GL (Fin 3) Real) (h: p ∈ G):
-       ∃ a b c : ℤ, ∃ n : ℕ, rotate p zero_one_zero = a_b_c_vec a b c n:=
-  Subgroup.closure_induction' h_s h_one h_mul h_inv h
+
+
+/-
+      simp
+      use 0
+      use 1
+      use 0
+      have h_empty : p = [] := by
+        exact List.length_eq_zero.mp h
+      rw [h_empty]
+      rw [list_to_matrix]
+      rw [rotate]
+      rw [coe_gl_one_eq_one]
+      simp [zero_one_zero, a_b_c_vec]
+    | succ n ih =>
+      sorry--/
