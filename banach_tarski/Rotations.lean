9bd8de29c999795fb4d9e1f487fb96f4e162d9d6
bergschaf <christian.k@netcom-mail.de>
2024-03-14 20:51:43 +0100

    commit

--- banach_tarski/Rotations.lean
+++ banach_tarski/Rotations.lean
@@ -1,5 +1,24 @@
 import banach_tarski.Definitions
 
+lemma tail_reduced_eq_reduced {α : Type} [DecidableEq α] (x : List (α × Bool)) (hx : x =  FreeGroup.reduce x) :
+    x.tail =  FreeGroup.reduce x.tail := by
+    induction x with
+    | nil =>
+        simp
+        exact hx
+    | cons head tail ih =>
+        simp
+        sorry
+
+
+
+
+
+
+
+
+
+
 lemma rotate_back_S_A (start : Set r_3) :
     rotate_set (rotate_set_around_set start S_A) gl_a' =
     (rotate_set_around_set start S_A) ∪ (rotate_set_around_set start S_B)
@@ -20,5 +39,23 @@ lemma rotate_back_S_A (start : Set r_3) :
     rcases h1 with ⟨x2,⟨rot, ⟨h_rot_reduced, rot_in_S_A⟩ , x_start, ⟨hx_start, h_start_mul⟩⟩,h1⟩
 
     simp [rotate_set_around_set, rotate]
-    use (erzeuger_t.gl_a,false)::rot
-    repeat sorry
+    use rot.tail
+    apply And.intro
+    . use (tail_reduced_eq_reduced rot h_rot_reduced)
+      sorry
+    . sorry
+
+    ---
+    intro h
+    cases h with
+    | inl h =>
+      cases h with
+      | inl h =>
+        sorry
+      | inr h =>
+        sorry
+
+    | inr h =>
+      simp [rotate_set_around_set, rotate_set, rotate] at *
+      rcases h with ⟨rot, ⟨h_rot_reduced, h_rot_in_SB⟩, x2, hx2_in_start, hx2_2⟩
+      sorry
