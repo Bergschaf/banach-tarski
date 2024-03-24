a0248f97b9c194878600be48e1ac3fdc67bab5f7
bergschaf <christian.k@netcom-mail.de>
2024-03-24 09:58:08 +0100

    s

--- banach_tarski.lean
+++ banach_tarski.lean
@@ -3,3 +3,17 @@ import banach_tarski.Lemma_3_2
 import banach_tarski.Equidecomposable.Equi_Kreis
 import banach_tarski.Equidecomposable.Equi_Ball
 import banach_tarski.Orbit
+import LeanCopilot
+
+import LeanCopilot
+
+def sum_up_to (n : Nat): ℕ :=
+  match n with
+    | 0 => 0
+    | (Nat.succ n) => sum_up_to n + Nat.succ n
+
+#eval sum_up_to 10
+
+theorem kleiner_gauss (n: Nat) :
+  sum_up_to n * 2 = n * (n + 1) := by
+  select_premises
