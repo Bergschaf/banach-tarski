315402ffd22fc2320143dea6a623d24a7739bb35
hetzner server <christian.k@netcom-mail.de>
2024-03-24 09:54:56 +0000

    Update ./andere-Theoreme/kleiner_gauss.lean

--- andere-Theoreme/kleiner_gauss.lean
+++ andere-Theoreme/kleiner_gauss.lean
@@ -1,6 +1,8 @@
 import Mathlib.Data.Nat.Basic
 import Mathlib.Data.Real.Basic
 
+import LeanCopilot
+
 def sum_up_to (n : Nat): ℕ :=
   match n with
     | 0 => 0
@@ -10,21 +12,4 @@ def sum_up_to (n : Nat): ℕ :=
 
 theorem kleiner_gauss (n: Nat) :
   sum_up_to n * 2 = n * (n + 1) := by
-
-  induction n with
-    | zero =>
-      rw [sum_up_to]
-
-    | succ d hd =>
-    rw [sum_up_to]
-    rw [Nat.succ_eq_add_one]
-    rw [Nat.add_mul]
-    rw [Nat.mul_add]
-    rw [Nat.mul_one]
-
-    rw [Nat.add_mul d 1 (d + 1)]
-    simp
-    rw [add_assoc]
-    rw [← Nat.mul_two (d + 1)]
-    simp
-    exact hd
+  
